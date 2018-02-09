f2efdb7dfe385842bcea421376bf29656ea4c546
Rainchess <2430153935@qq.com>
2018-02-09 17:50:47 +0800

    教育页面

--- src/components/education.vue
+++ src/components/education.vue
@@ -10,11 +10,26 @@
                 <li>技能</li>
             </ul>
         </div>
+        <div class="head">
+            <img class="circleImg" src="../assets/imgs/head1.jpg">
+        </div>
+        <div class="container">
+            <p class="title">教 育</p>
+            <hr class="hrTop"/>
+            <hr class="hrBottom"/>
+        </div>
     </div>
 </template>
 
 <script>
+    export default {
+        name: 'Education',
+        data () {
+            return{
 
+            }
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -37,5 +52,35 @@
         margin-left: 90px;
         font-size: 20px;
         text-align: center;
+    }
+    .head{
+        text-align: center;
+        padding-top: 150px;
+    }
+    .circleImg{
+        border-radius: 70px;
+        border: 3px solid #EAEAEA;
+    }
+    .container{
+        width: 800px;
+        height: 500px;
+        background-color: #EAEAEA;
+        margin: auto;
+        margin-top: -70px;
+        margin-bottom: 100px;
+    }
+    .title{
+        font-size: 30px;
+        padding-top: 100px;
+        text-align: center;
+        color: grey;
+    }
+    hr{
+        width: 100px;
+        height: 1px;
+        margin-left: 350px;
+    }
+    .hrTop{
+        margin-top: 20px;
     }
 </style>
